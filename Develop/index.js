1f612db109734e2af1307a375ef3c819b3e67280
Snookie <kieransnook35@gmail.com>
2020-03-25 19:49:03 -0400

    added questions and wrote then function for consolelog

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,42 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your GitHub name?",
+      name: "gitname"
+    },
+    {
+        type:"input",
+        message:"What is your project called?",
+        name:"projectName"
+    },
+    {
+        type: "input",
+        message:"Write a short description of your project (why)",
+        name: "description"
+    },
+    { 
+        type: "list",
+        message: "Which license are you going to attatch to your project?",
+        name: "license",
+        choices:
+         ["MIT",
+         "APACHE 2.0",
+         "GPL 3.0",
+         "BSD 3",
+         "None"]
 
-function writeToFile(fileName, data) {
-}
+    },
+    {
+        type: "input",
+        message: "Contributions?",
+        name: "contributions",
+        default: "None"
+    },
+  ]).then(function(response) {console.log(response);
 
-function init() {
-
-}
-
-init();
+  });
